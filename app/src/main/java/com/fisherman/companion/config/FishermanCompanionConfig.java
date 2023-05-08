21b969c3eb7e69fbfcad58073d5f7b9e05efc1cc
Roman Krutsko <roman.krutsko@betpawa.com>
2023-05-09 01:34:58 +0300

    Deployment setup
    * try component scan on lower level

--- app/src/main/java/com/fisherman/companion/config/FishermanCompanionConfig.java
+++ app/src/main/java/com/fisherman/companion/config/FishermanCompanionConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.fisherman.companion.rest")
+@ComponentScan("com.fisherman.companion")
 public class FishermanCompanionConfig {
 }
