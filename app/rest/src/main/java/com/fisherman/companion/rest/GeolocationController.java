2946f1a16ca1b3b8d56cef5d8ff44330ddbbce05
Roman Krutsko <roman.krutsko@betpawa.com>
2023-05-19 18:25:56 +0300

    Redo logic of user and profile
    * add call for getting coordinates from settlement name
    * change url paths and method types

--- app/rest/src/main/java/com/fisherman/companion/rest/GeolocationController.java
+++ app/rest/src/main/java/com/fisherman/companion/rest/GeolocationController.java
@@ -1,11 +1,11 @@
 package com.fisherman.companion.rest;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fisherman.companion.dto.request.GeoAutocompleteRequest;
+import com.fisherman.companion.dto.Geolocation;
 import com.fisherman.companion.dto.response.GenericListResponse;
 import com.fisherman.companion.service.GeolocationService;
 
@@ -13,12 +13,17 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/geo")
+@RequestMapping("/location")
 public class GeolocationController {
     private final GeolocationService geolocationService;
 
-    @PostMapping("/autocomplete")
-    GenericListResponse<String> autocompleteSettlements(@RequestBody GeoAutocompleteRequest geoAutocompleteRequest) {
-        return geolocationService.getAutocompleteSettlements(geoAutocompleteRequest.settlement());
+    @GetMapping
+    Geolocation getSettlementCoordinates(@RequestParam(value = "settlement") final String settlement) {
+        return geolocationService.getCoordinates(settlement);
+    }
+
+    @GetMapping("/autocomplete")
+    GenericListResponse<String> autocompleteSettlements(@RequestParam(value = "settlement") final String settlement) {
+        return geolocationService.getAutocompleteSettlements(settlement);
     }
 }
